0224b6bb9a9e9cfca8e2f9b966426ef6c0fac801
Shaked Levnat <shaked.levnat.work@gmail.com>
2024-09-14 22:29:11 +0300

    getting results need to add db and write to file

--- backendserver/AI.py
+++ backendserver/AI.py
@@ -1,16 +1,17 @@
 import sys
 import json
+import requests
+
+attack_keys = {"id", "description", "phase name", "name", "platform", "detection"}
+encryption_types = {"md5", "sha1", "sha256", "sha512", "bcrypt", "aes", "rsa"}
+url_like_words = {"url", "website", "link"}
+ip_address_like_words = {"ip", "ipaddress"}
+file_hash_words = {"hash", "checksum", "filehash"}
+
+
+all_keys = attack_keys | encryption_types | url_like_words | ip_address_like_words | file_hash_words
 
 def extract_key_value(sentence):
-    attack_keys = {"id", "description", "phase name", "name", "platform", "detection"}
-    encryption_types = {"md5", "sha1", "sha256", "sha512", "bcrypt", "aes", "rsa"}
-    url_like_words = {"url", "website", "link"}
-    ip_address_like_words = {"ip", "ipaddress"}
-    file_hash_words = {"hash", "checksum", "filehash"}
-
-
-    all_keys = attack_keys | encryption_types | url_like_words | ip_address_like_words | file_hash_words
-
     results = {}
     found_attack_keys = set()
     words = sentence.lower().split()
@@ -49,18 +50,40 @@ def extract_key_value(sentence):
 
     return results
 
-def main():
-    args = sys.argv[1:]
-    try:
-        if not args:
-            raise ValueError("No input sentence provided")
-        sentence = args[0]
+def getVirustotalResults(results: dict[str, str]):
+    apikey: str = '3704563ee024370204fca6814514fe33d0713f10b4385bbd9d4d8f552fd2fec0'
+    for key, value in results.items():
+        if key not in attack_keys:
+            url = 'https://www.virustotal.com/vtapi/v2/file/report'
+            params = {
+                'apikey': apikey,
+                'resource': value
+            }
 
-        key_value_pairs = extract_key_value(sentence)
+            response = requests.get(url, params=params)
+            data = response.json()
 
-        with open('output.txt', 'w') as file:
-            json.dump(key_value_pairs, file, indent=3)
+            return data.get('positives', 0) == 0
     
+    return False
+
+def main():
+    #args = sys.argv
+    #if len(args) > 1:
+        #args = args[1:]
+    try:
+        #if not args or len(args) <= 1:
+            #sentence = args[0]
+
+            #raise ValueError("No input sentence provided")
+        
+        sentence = 'md5 5d41402abc4b2a76b9719d911017c592'
+        results = extract_key_value(sentence)
+        with open('output.txt', 'w') as file:
+            result = str(getVirustotalResults(results))
+            print(result)
+            file.write('results')
+
     except Exception as e:
         error_message = f"Error occurred: {str(e)}"
         with open('output.txt', 'w') as file:
